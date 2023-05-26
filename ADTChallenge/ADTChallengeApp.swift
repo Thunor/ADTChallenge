50d6f32f03a82a05e4330cbd450bc6e3f2e93768
Eric Freitas <efreitos@gmail.com>
2023-05-26 13:12:26 -0400

    Initial Commit

---
+++ ADTChallenge/ADTChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADTChallengeApp.swift
+//  ADTChallenge
+//
+//  Created by Eric Freitas on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ADTChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
