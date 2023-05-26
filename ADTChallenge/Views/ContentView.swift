c1fc0dac468838b92d42402afb9f34ec4b389a02
Eric Freitas <efreitos@gmail.com>
2023-05-26 16:59:55 -0400

    updated view colors

--- ADTChallenge/Views/ContentView.swift
+++ ADTChallenge/Views/ContentView.swift
@@ -13,45 +13,52 @@ struct ContentView: View {
     @State private var currentPage: Int = 1
     
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView {
-                    ForEach(episodes ?? [], id: \.id) { episode in
-                        NavigationLink {
-                            EpisodeDetailView(detail: episode)
-                        } label: {
-                            HStack {
-                                Text(episode.name ?? "")
-                                    .foregroundColor(.white)
-                                Spacer()
-                                Image(systemName: "chevron.right")
-                                    .fontWeight(.heavy)
-                            }
-                            .caption(backgroundColor: .orange)
-                        }
-                        .onBecomingVisible {
-                            if (episode.id ?? 1) % 20 == 0 {
-                                if info?.info?.next != nil {
-                                    print("id: \(episode.id ?? 0)")
-                                    currentPage += 1
-                                    loadMore()
-                                } else {
-                                    print("No more data..")
+            
+            NavigationView {
+                ZStack {
+                    Color.init(red: 0.8, green: 0.8, blue: 0.8)
+                        .ignoresSafeArea()
+                    
+                    VStack {
+                        ScrollView {
+                            ForEach(episodes ?? [], id: \.id) { episode in
+                                NavigationLink {
+                                    EpisodeDetailView(detail: episode)
+                                } label: {
+                                    HStack {
+                                        Text(episode.name ?? "")
+                                            .foregroundColor(.white)
+                                        Spacer()
+                                        Image(systemName: "chevron.right")
+                                            .fontWeight(.heavy)
+                                    }
+                                    .caption(backgroundColor: .gray)
+                                }
+                                .onBecomingVisible {
+                                    if (episode.id ?? 1) % 20 == 0 {
+                                        if info?.info?.next != nil {
+                                            print("id: \(episode.id ?? 0)")
+                                            currentPage += 1
+                                            loadMore()
+                                        } else {
+                                            print("No more data..")
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+                    .padding()
+                }
+                .onAppear {
+                    RMAPI.getEpisodes(page: 1, completion: { model, theInfo, message in
+                        episodes = model?.results
+                        info = theInfo
+                        print("added first page")
+                    })
                 }
             }
-            .padding()
-            .onAppear {
-                RMAPI.getEpisodes(page: 1, completion: { model, theInfo, message in
-                    episodes = model?.results
-                    info = theInfo
-                    print("added first page")
-                })
-            }
-        }
+            .background(Color.teal)
     }
     
     /// Load next page of data
--- ADTChallenge/Views/EpisodeDetailView.swift
+++ ADTChallenge/Views/EpisodeDetailView.swift
@@ -11,33 +11,83 @@ struct EpisodeDetailView: View {
     @State var detail: Episode?
     
     var body: some View {
-        VStack {
-            Text(detail?.episode ?? "*")
-                .caption()
+        ZStack {
+            
+            Color.init(red: 0.8, green: 0.8, blue: 0.8)
+                .ignoresSafeArea()
+            
+            VStack(content: {
+                HStack {
+                    Text("episode: ")
+                    Spacer()
+                    Text("\(detail?.episode ?? "")")
+                }
+                .padding(10)
+                .cornerRadius(6.0)
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("name: ")
+                    Spacer()
+                    Text("\(detail?.name ?? "")")
+                }
+                .padding(10)
+                .cornerRadius(6.0)
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("air date: ")
+                    Spacer()
+                    Text("\(detail?.airDate ?? "")")
+                }
+                .padding(10)
+                .cornerRadius(6.0)
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("creation date: ")
+                    Spacer()
+                    Text("\(detail?.created ?? "")")
+                }
+                .padding(10)
+                .cornerRadius(6.0)
+                .padding(.horizontal)
+                
+                // I would display a CharacterList() here
+                
+                Spacer()
+            })
+            .foregroundColor(.white)
         }
+        .accentColor(.orange)
         .onAppear {
             RMAPI.getEpisodeDetail(episodeID: detail?.id ?? 0) { episode, message in
                 detail = episode
                 print("got detail")
             }
+            
+            // Need to get the Character array here, using the detail.character endpoints,
+            // then it would populate the CharacterList() view.  In that view, I would, as a
+            // minimum show the character name and image, with other data as needed.
         }
     }
 }
 
 struct EpisodeDetailView_Previews: PreviewProvider {
-    var epDetail: Episode? = Episode(id: 1,
-                         name: "Pilot",
-                         airDate: "December 2, 2013",
-                         episode: "S01E01",
-                         characters: [
-                            "https://rickandmortyapi.com/api/character/1",
-                            "https://rickandmortyapi.com/api/character/2"
-                         ],
-                         url: "https://rickandmortyapi.com/api/episode/1")
     
     static var previews: some View {
-//        epi
-        EpisodeDetailView()
-//        EpisodeDetailView(detail: epDetail)
+        let epDetail: Episode? = Episode(
+            id: 1,
+            name: "Pilot",
+            airDate: "December 2, 2013",
+            episode: "S01E01",
+            characters: [
+                "https://rickandmortyapi.com/api/character/1",
+                "https://rickandmortyapi.com/api/character/2"
+            ],
+            url: "https://rickandmortyapi.com/api/episode/1",
+            created: "2017-11-10T12:56:33.798Z"
+        )
+        EpisodeDetailView(detail: epDetail)
     }
 }
